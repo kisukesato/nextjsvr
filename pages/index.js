0735d7dbb775cc6da720e7486724837b9882d22b
kisukesato <65474365+kisukesato@users.noreply.github.com>
2021-03-25 10:55:17 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 const Index = () => (
-  <div>
-    <p>Hello Next.js</p>
-  </div>
+<a-scene embedded arjs="debugUIEnabled:false; sourceType: webcam;"> // ※1
+        <a-marker preset="custom" type='pattern' url="my-icon-marker.patt"> // ※2
+            <a-text value="My name is soeyu!\n Nice to meet you!" position=" 0 0 1" align="center" rotation="-90 0 0" color="#7993ff"> // ※3
+            </a-text>
+        </a-marker>
+    <a-entity camera></a-entity>
+</a-scene>
 )
 
 export default Index
